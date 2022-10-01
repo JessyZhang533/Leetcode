e1ce8867afcfbf887a96bc948112c1f056980ce1
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-10-01 23:10:43 +0100

    91

---
+++ Q91.py
@@ -0,0 +1,20 @@
+# Decode ways
+# DYNAMIC PROGRAMMIN
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # Edge case check
+        if s is None or s[0] == '0':
+            return 0
+
+        # dp[i] is the ways to parse s[:i]
+        dp = [1] * len(s)
+        for i in range(1, len(s)): 
+            # One digit check
+            dp[i] = 0 if int(s[i]) == 0 else dp[i - 1]
+            # Two digit check
+            if 10 <= int(s[i-1:i+1]) <= 26:
+                dp[i] += dp[i - 2 if i > 1 else 0]   
+        # Return the last element
+        return dp[-1]
+
