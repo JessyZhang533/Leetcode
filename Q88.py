b4bf654c2deedfc78dde2c081f5f067ccbcc59d2
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-10-03 01:36:03 +0100

    88

---
+++ Q88.py
@@ -0,0 +1,17 @@
+# Merge sorted arrays
+
+# From max value to min value
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while m > 0 and n > 0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if n > 0:  # Don't need case where m>0, cause then we don't need to modify nums1
+            nums1[:n] = nums2[:n]
