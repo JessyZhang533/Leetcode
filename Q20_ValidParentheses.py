e8f9b985fed7aaffab61cb4147edcf7a986d3bd7
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-10-16 18:14:08 +0100

    review

--- Q20_ValidParentheses.py
+++ Q20_ValidParentheses.py
@@ -11,7 +11,7 @@ class Solution:
             if char in dict.values():
                 stack.append(char)
             elif char in dict.keys():
-                if stack == [] or dict[char] != stack.pop():
+                if stack == [] or dict[char] != stack.pop():  # stack is modified here (popped last item)
                     return False
             else:
                 return False
