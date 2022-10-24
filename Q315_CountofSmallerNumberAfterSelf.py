7f4bf96269304061407bac7dc0a614921c1f5765
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-10-24 22:08:10 +0100

    comment

--- Q315_CountofSmallerNumberAfterSelf.py
+++ Q315_CountofSmallerNumberAfterSelf.py
@@ -3,7 +3,7 @@ class Solution:
         def sort(indexes):
             half = len(indexes) // 2
             if half:
-                left, right = sort(indexes[:half]), sort(indexes[half:])
+                left, right = sort(indexes[:half]), sort(indexes[half:])  # recursive
                 for i in range(len(indexes))[::-1]:
                     if not right or left and nums[left[-1]] > nums[right[-1]]:
                         smaller[left[-1]] += len(right)
