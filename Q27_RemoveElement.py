5d82952130735d9654a355510a08d0dd02db8e10
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-10-25 23:52:56 +0100

    27

---
+++ Q27_RemoveElement.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeElement_1(self, nums: List[int], val: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == val:
+                j = i
+                while nums[j] == val:
+                    if j == len(nums) - 1:
+                        return i
+                    j += 1
+                nums[i], nums[j] = nums[j], nums[i]
+
+    def removeElement_2(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] == val:
+                continue
+            nums[k] = nums[i]
+            k += 1
+        return k
