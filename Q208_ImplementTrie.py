784fe5c17d02767b4428eb66b3b49a2eefa4dbbb
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-10-31 00:10:10 +0000

    208

--- Q208_ImplementTrie.py
+++ Q208_ImplementTrie.py
@@ -14,22 +14,22 @@ class Trie:
     # @return {void}
     # Inserts a word into the trie.
     def insert(self, word):
-        node=self.root
+        node = self.root
         for i in word:
             if i not in node.children:
-                node.children[i]=TrieNode()
-            node=node.children[i]
-        node.word=True
+                node.children[i] = TrieNode()
+            node = node.children[i]
+        node.word = True
 
     # @param {string} word
     # @return {boolean}
     # Returns if the word is in the trie.
     def search(self, word):
-        node=self.root
+        node = self.root
         for i in word:
             if i not in node.children:
                 return False
-            node=node.children[i]
+            node = node.children[i]
         return node.word
 
     # @param {string} prefix
@@ -37,11 +37,11 @@ class Trie:
     # Returns if there is any word in the trie
     # that starts with the given prefix.
     def startsWith(self, prefix):
-        node=self.root
+        node = self.root
         for i in prefix:
             if i not in node.children:
                 return False
-            node=node.children[i]
+            node = node.children[i]
         return True
 
 
