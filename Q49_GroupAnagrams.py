637be9554e1e839ccf5a0df9bdb1b47e6bd5ca10
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-11-08 22:34:28 +0000

    49 review

--- Q49_GroupAnagrams.py
+++ Q49_GroupAnagrams.py
@@ -6,5 +6,5 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         letters_to_words = defaultdict(list)
         for word in strs:
-            letters_to_words[tuple(sorted(word))].append(word)  # 'tuple(sorted(word))' is a tuple of separate letters
+            letters_to_words[tuple(sorted(word))].append(word)  # 'tuple(sorted(word))' is a tuple of separate letters; need to convert to tuples because it is hashable but lists are not
         return list(letters_to_words.values())
