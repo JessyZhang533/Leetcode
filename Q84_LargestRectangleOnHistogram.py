46eaace7b424b30958e09dd2e3602954cb6e8bf4
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-11-08 21:22:45 +0000

    84 & review

---
+++ Q84_LargestRectangleOnHistogram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        " for given bar i with value h, how to find the biggest rectangle, which is ending with this bar?  "
+        stack, ans = [], 0
+        for i, h in enumerate(heights + [0]):
+            while stack and heights[stack[-1]] >= h:
+                H = heights[stack.pop()]
+                W = i if not stack else i-stack[-1]-1
+                ans = max(ans, H*W)
+            stack.append(i)
+        return ans
