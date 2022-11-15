6f072f78229d1684ecd629463def3b800b44ce00
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-11-15 13:31:42 +0000

    comments

--- Q1048_LongestStringChain.py
+++ Q1048_LongestStringChain.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        dp = {}
-        for w in sorted(words, key=len):
-            dp[w] = max(dp.get(w[:i] + w[i + 1:], 0) + 1 for i in range(len(w)))
+        dp = {}  # key: words; value: length of word chain
+        for w in sorted(words, key=len):  # firstly, dort the word list by length
+            dp[w] = max(dp.get(w[:i] + w[i + 1:], 0) + 1 for i in range(len(w)))  # dictionary.get()
         return max(dp.values())
--- Q166_FractionToRecurringDecimal.py
+++ Q166_FractionToRecurringDecimal.py
@@ -4,16 +4,21 @@ class Solution:
         n, remainder = divmod(abs(numerator), abs(denominator))
         sign = '-' if numerator*denominator < 0 else ''
         result = [sign+str(n), '.']
-        remainders = {}
+        remainders = {}  # key: remainder; value: number of digits + 1 (this is for '.')
 
-        while remainder > 0 and remainder not in remainders:
+        while remainder > 0 and remainder not in remainders:  # the second condition: once the digits start repeating (recursive decimals), exit the while loop
             remainders[remainder] = len(result)
-            n, remainder = divmod(remainder*10, abs(denominator))
+            n, remainder = divmod(remainder*10, abs(denominator))  # calculate the next digit: remainder*10
             result.append(str(n))
 
-        if remainder in remainders:
+        if remainder in remainders:  # for non-recursive decimals, now 'remainder'=0, and it is not in 'remainders'
             idx = remainders[remainder]
             result.insert(idx, '(')
             result.append(')')
 
-        return ''.join(result).rstrip(".")
+        return ''.join(result).rstrip(".")  # .rstrip() get rid of '.' if result is an integer
+
+
+# 1.rstrip(): returns a copy of the string in which all chars have been stripped from the end of the string (default whitespace characters).
+# 2.divmod(num, den): returns the quotient and remainder as a tuple
+# 3.join(): takes all items in an iterable and joins them into one string
