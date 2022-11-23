88417fc469f41f7d528972184636564a25857f93
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-11-23 23:47:38 +0000

    300

---
+++ Q300_LongestIncreasingSequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:  # t.c. O(N^2)
+        n = len(nums)
+        dp = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+        return max(dp)
+
+    # bisect_left():  finds and returns the position at which an element can be inserted into a Python list while maintaining the sorted order of the Python list
+    def lengthOfLIS(self, nums: List[int]) -> int:  # t.c. O(NlogN)
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                idx = bisect_left(sub, x)  # Find the index of the first element >= x
+                sub[idx] = x  # Replace that number with x
+        return len(sub)
