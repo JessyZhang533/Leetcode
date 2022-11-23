882d13409e2ea3adc470da8d00375049e8023b0a
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-11-23 23:51:59 +0000

    300

--- Q300_LongestIncreasingSequence.py
+++ Q300_LongestIncreasingSequence.py
@@ -1,5 +1,8 @@
+import bisect
+
+
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:  # t.c. O(N^2)
+    def lengthOfLIS_1(self, nums: List[int]) -> int:  # t.c. O(N^2)
         n = len(nums)
         dp = [1] * n
         for i in range(n):
@@ -9,12 +12,12 @@ class Solution:
         return max(dp)
 
     # bisect_left():  finds and returns the position at which an element can be inserted into a Python list while maintaining the sorted order of the Python list
-    def lengthOfLIS(self, nums: List[int]) -> int:  # t.c. O(NlogN)
+    def lengthOfLIS_2(self, nums: List[int]) -> int:  # t.c. O(NlogN)
         sub = []
         for x in nums:
             if len(sub) == 0 or sub[-1] < x:
                 sub.append(x)
             else:
-                idx = bisect_left(sub, x)  # Find the index of the first element >= x
+                idx = bisect.bisect_left(sub, x)  # Find the index of the first element >= x
                 sub[idx] = x  # Replace that number with x
         return len(sub)
