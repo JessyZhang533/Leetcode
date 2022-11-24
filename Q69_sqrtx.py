981f11157fbc24cc9e85665a38c75a38c59bd135
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-11-24 23:50:33 +0000

    69

---
+++ Q69_sqrtx.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        " binary search "
+        if x==1:
+            return 1 #deal with exception
+        l, r = 0, x
+        while l <= r:
+            mid = (r+l)//2
+            if mid * mid <= x < (mid+1)*(mid+1):
+                return mid
+            elif x < mid * mid:
+                r = mid
+            else:
+                l = mid
