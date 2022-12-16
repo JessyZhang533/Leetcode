17b894efe39221b4175c0400f617fb361616ad36
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-12-16 00:23:26 +0000

    comments

--- Q190_ReverseBits.py
+++ Q190_ReverseBits.py
@@ -1,9 +1,10 @@
 class Solution:
+    # bitwise operators https://realpython.com/python-bitwise-operators/
     def reverseBits(self, n: int) -> int:
         ret = 0
         for i in range(16):
-            right_bit = (n>>i)&1
+            right_bit = (n>>i)&1  # equals 1 if last bit of n>>i is 1; equals 0 if last bit of n>>i is 0
             left_bit = (n>>(31-i))&1
             ret |= right_bit<<(31-i)
             ret |= left_bit<<i
-        return ret  
+        return ret 
