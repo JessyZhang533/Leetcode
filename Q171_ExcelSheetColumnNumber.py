ba4aae816bad29a1c50ab34254d5ae61023f2a8b
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-12-21 00:21:52 +0000

    171 my solution

--- Q171_ExcelSheetColumnNumber.py
+++ Q171_ExcelSheetColumnNumber.py
@@ -1,9 +1,15 @@
 class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
+    def titleToNumber_1(self, columnTitle: str) -> int:
+        res, n = 0, 0
+        while n < len(columnTitle):
+            res = res + (ord(columnTitle[n]) - ord('A') + 1) * 26**(len(columnTitle)-n-1)
+            n += 1
+        return res
+
+    def titleToNumber_2(self, columnTitle: str) -> int:
         ans, pos = 0, 0
         for letter in reversed(columnTitle):
             digit = ord(letter)-64
             ans += digit * 26**pos
             pos += 1
-            
         return ans
