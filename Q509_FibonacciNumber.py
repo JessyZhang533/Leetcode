d9c3a22eb588561b467ecde0366398efc2947bee
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-01-01 23:54:28 +0000

    509

---
+++ Q509_FibonacciNumber.py
@@ -0,0 +1,17 @@
+class Solution_1:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return self.fib(n-1) + self.fib(n-2)
+
+
+# below much faster
+from functools import lru_cache
+class Solution_2:
+    @lru_cache(maxsize=None)
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        return self.fib(n-1) + self.fib(n-2)
