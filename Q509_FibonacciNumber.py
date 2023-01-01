de478220a6f2e0db2a271117530fcba17a3daf2b
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-01-01 23:56:09 +0000

    509

--- Q509_FibonacciNumber.py
+++ Q509_FibonacciNumber.py
@@ -10,7 +10,7 @@ class Solution_1:
 # below much faster
 from functools import lru_cache
 class Solution_2:
-    @lru_cache(maxsize=None)
+    @lru_cache(maxsize=None)  # lru_cache() is a decorator that helps in reducing function execution for the same inputs using the memoization technique
     def fib(self, n: int) -> int:
         if n < 2:
             return n
