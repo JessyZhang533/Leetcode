dba140c0432b83264c742eeaf776dd703ada05a2
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-01-02 20:23:17 +0000

    70 more

--- Q70_ClimbingStairs.py
+++ Q70_ClimbingStairs.py
@@ -1,4 +1,6 @@
-# Basically Fibonacci
+# Basically Fibonacci (Q509, Q1137)
+from functools import lru_cache
+
 
 class Solution:
     def climbStairs_1(self, n):
@@ -25,3 +27,18 @@ class Solution:
         for _ in range(n):
             a, b = b, a + b
         return a
+
+    def climbStairs_4(self, n):
+        " Dynamic programming; see Q1137 "
+        dp = [1, 2]
+        for i in range(2, n):  # note start and end!
+            dp[i % 2] = sum(dp)  # i or i-1 or i+1?
+            print(dp)
+        return dp[(n+1) % 2]  # n+1 not n
+
+    @lru_cache
+    def climbStairs_5(self, n):
+        if n < 3:
+            return n
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        
