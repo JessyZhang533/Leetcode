23116b8160b51105b6988ec237917d6c927de991
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-01-02 00:46:42 +0000

    comments

--- Q509_FibonacciNumber.py
+++ Q509_FibonacciNumber.py
@@ -15,3 +15,23 @@ class Solution_2:
         if n < 2:
             return n
         return self.fib(n-1) + self.fib(n-2)
+
+
+# https://realpython.com/lru-cache-python/
+
+# Wrap:
+# What does wrapping mean in programming?
+# "Wrap" is a standard English word meaning "Cover or enclose". Typically programmers use it to mean enclosing the functionality of something with something else
+
+# Memorization/Caching:
+# This approach ensures that a function doesn’t run for the same inputs more than once by storing its result in memory and then referencing it later when necessary. 
+
+# Least Recently Used (LRU):
+# A cache implemented using the LRU strategy organizes its items in order of use. Every time you access an entry, the LRU algorithm will move it to the top of the cache.
+# This way, the algorithm can quickly identify the entry that’s gone unused the longest by looking at the bottom of the list.
+# The LRU strategy assumes that the more recently an object has been used, the more likely it will be needed in the future, so it tries to keep that object in the cache for the longest time.
+
+# lru_cache:
+# Python’s @lru_cache decorator offers a maxsize attribute that defines the maximum number of entries before the cache starts evicting old items.
+# By default, maxsize is set to 128. If you set maxsize to None, then the cache will grow indefinitely, and no entries will be ever evicted.
+# This could become a problem if you’re storing a large number of different calls in memory.
