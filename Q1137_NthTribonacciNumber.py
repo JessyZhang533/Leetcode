0e7a2b2dda52209489549bd66cc976f31f084eb9
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-01-02 00:57:00 +0000

    1137 + progress file

--- Q1137_NthTribonacciNumber.py
+++ Q1137_NthTribonacciNumber.py
@@ -1,5 +1,7 @@
 from functools import lru_cache
-class Solution:
+
+
+class Solution_1:
     @lru_cache
     def tribonacci(self, n: int) -> int:
         if n < 2:
@@ -7,3 +9,12 @@ class Solution:
         if n == 2:
             return 1
         return self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)
+
+
+class Solution_2:
+    # Dynamic programming
+    def tribonacci(self, n):
+        dp = [0, 1, 1]
+        for i in range(3, n + 1):
+            dp[i % 3] = sum(dp)  # !!!
+        return dp[n % 3]
