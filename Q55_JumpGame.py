6512742b6a24f5c29353074e2d0efd99fec78ad0
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-01-04 21:47:09 +0000

    55

--- Q55_JumpGame.py
+++ Q55_JumpGame.py
@@ -6,3 +6,16 @@ class Solution:
             if (i + nums[i]) >= last_pos:
                 last_pos = i
         return last_pos == 0
+
+    # time limit exceeded, but intruduces a top-down dp solution
+    # dp[i] represents whether we can reach the end of list
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [False]*n
+        dp[-1] = True
+        for i in range(n-2, -1, -1):
+            for j in range(i+1, min(n, i+nums[i]+1)):  # check indices (i+1) --> (n-1) or (i+nums[i])
+                if dp[j]:  # if any entries in the above range is True, then it means we can reach the end of list from d[i]
+                    dp[i] = True
+                    break
+        return dp[0]
