a84a9b6760f6fe7ffd7be1038dc293a5b40d34a8
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-01-07 12:10:31 +0000

    121

--- Q121_BuyandSellStock.py
+++ Q121_BuyandSellStock.py
@@ -1,7 +1,5 @@
-# Buy and Sell Stock
-
 class Solution:
-    def maxProfit(self,prices):
+    def maxProfit_1(self,prices):
         # two pointers
         left = 0 #Buy
         right = 1 #Sell
@@ -14,3 +12,13 @@ class Solution:
                 left = right
             right += 1
         return max_profit
+
+    def maxProfit_2(self,prices):
+        " Q1014 method 2 & 3; the only difference is that we can directly operate with original values here "
+        dp = [0]*len(prices)
+        dp[0] = prices[0]
+        res = 0
+        for i in range(1, len(prices)):
+            dp[i] = min(dp[i-1], prices[i-1])
+            res = max(res, prices[i]-dp[i])
+        return res
