7e0a43002621fb7329a48c3e90676ab4f4da7e4e
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-01-07 12:13:34 +0000

    122

--- Q122_BuyAndSellStock2.py
+++ Q122_BuyAndSellStock2.py
@@ -1,20 +1,25 @@
 class Solution:
     def maxProfit_1(self, prices: List[int]) -> int:
-		# It is impossible to sell stock on first day, set -infinity as initial value for cur_hold
         cur_hold, cur_not_hold = -float('inf'), 0
-
         for stock_price in prices:
-            prev_hold, prev_not_hold = cur_hold, cur_not_hold
-			# either keep hold, or buy in stock today at stock price
-            cur_hold = max(prev_hold, prev_not_hold - stock_price)
-			# either keep not-hold, or sell out stock today at stock price
-            cur_not_hold = max(prev_not_hold, prev_hold + stock_price)
-        # maximum profit must be in not-hold state
+            cur_hold, cur_not_hold = max(cur_hold, cur_not_hold - stock_price), max(cur_not_hold, cur_hold + stock_price)
         return cur_not_hold
 
     def maxProfit_2(self, prices: List[int]) -> int:
-        price_gain = []
-        for idx in range(len(prices)-1):
-            if prices[idx] < prices[idx+1]:
-                price_gain.append(prices[idx+1]- prices[idx])
+        " same idea as method 3; but with a dp list (?) "
+        price_gain = []  # store all positive (prices[i+1]-prices[i]) values
+        for i in range(len(prices)-1):
+            if prices[i] < prices[i+1]:
+                price_gain.append(prices[i+1]- prices[i])
         return sum(price_gain)
+
+    def maxProfit_3(self, prices: List[int]) -> int:
+        " from day X, the buying day will be the last continuous day that the price is smallest. "
+        prices.append(0)
+        profit = 0
+        buy_price = prices[0]
+        for i in range(len(prices)):
+            if prices[i] < prices[i - 1]:
+                profit += prices[i - 1] - buy_price
+                buy_price = prices[i]
+        return profit
