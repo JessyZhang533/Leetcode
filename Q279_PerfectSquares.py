80acf34f45c8c22ffe8d0a416d30a1226672dbbe
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2023-01-21 23:50:09 +0000

    279

--- Q279_PerfectSquares.py
+++ Q279_PerfectSquares.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numSquares(self, n: int) -> int:
+    def numSquares_1(self, n: int) -> int:
         " BFS "
         if n < 2:  # handle edge case
             return n
@@ -25,3 +25,14 @@ class Solution:
             toCheck = temp  # set toCheck becomes set temp
 
         return cnt
+
+    def numSquares_2(self, n: int) -> int:
+        " dp[i] is the the least number of perfect square numbers that sum to i "
+        dp = [float('inf')]*(n+1)
+        dp[0] = 0
+        for i in range(1, n+1):
+            j = 1
+            while j*j <= i:  # use a while loop when you can't decide the upper limit
+                dp[i] = min(dp[i], dp[i-j*j]+1)
+                j+=1  # !!! don't forget
+        return dp[-1]
